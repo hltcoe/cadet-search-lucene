5d1a33eb8ef868fd1d12893e2b2911d98002b236
Craig Harman <craig@craigharman.net>
2016-10-19 11:17:16 -0400

    Corrected class name in start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 DIR=`dirname $0`
 JAR=$(find $DIR/target/ -name 'cadet-search-lucene*.jar')
-java -cp $JAR:target/dependency/* edu.jhu.hlt.cadet.search.Search "$@"
+java -cp $JAR:target/dependency/* edu.jhu.hlt.cadet.search.Server "$@"
