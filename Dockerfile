c63ebec9ae7a3cb36b92c50954b1694566e4938b
Craig Harman <craig@craigharman.net>
2017-11-01 15:51:07 -0400

    Docker container now waits for Fetch to come up before starting search server

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM maven
 
+# netcat is used by docker-entrypoint.sh to test if the Fetch server is up
+RUN apt-get update && apt-get install -y \
+    netcat \
+  && rm -rf /var/lib/apt/lists/*
+
 COPY pom.xml /opt/cadet-search-lucene/pom.xml
 COPY src /opt/cadet-search-lucene/src
+COPY wait-for-fetch.sh /opt/cadet-search-lucene/wait-for-fetch.sh
 
 WORKDIR /opt/cadet-search-lucene
 RUN mvn -B clean package \
@@ -11,6 +17,6 @@ RUN mvn -B clean package \
         cadet-search-lucene.jar && \
     mvn -B clean
 
-ENTRYPOINT [ "java", "-jar", "cadet-search-lucene.jar" ]
+ENTRYPOINT [ "./wait-for-fetch.sh", "java", "-jar", "cadet-search-lucene.jar" ]
 
 CMD ["--help"]
---
+++ wait-for-fetch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+TIMEOUT=30
+
+# Adapted from:
+#   https://github.com/Eficode/wait-for
+
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1
+    
+    result=$?
+    if [ $result -eq 0 ] ; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
+      fi
+      exit 0
+    fi
+    echo "Waiting for Fetch server to come up..." >&2
+    sleep 1
+  done
+  echo "Timed out waiting for Fetch server" >&2
+  exit 1
+}
+
+argc=$#
+argv=($@)
+
+for (( j=0; j<argc; j++ )); do
+    # Check if there is an '--fh' arg that is not the last arg
+    if [ ${argv[j]} == "--fh" ] && [ $((j)) -lt $((argc-1)) ]; then
+	# Set HOST to arg after '--fh'
+	HOST=${argv[j+1]}
+    fi
+
+    # Check if there is an '--fp' arg that is not the last arg
+    if [ ${argv[j]} == "--fp" ] && [ $((j)) -lt $((argc-1)) ]; then
+	# Set PORT to arg after '--fp'
+	PORT=${argv[j+1]}
+    fi
+done
+
+if [ "$HOST" == "" -o "$PORT" == "" ]; then
+    echo "Error: you must specify a Fetch server using '--fp PORT' and '--fh HOST'" >&2
+    exit 2
+fi
+
+wait_for "$@"
